bd3531886ecf4e928cd3f61b7f21752cc4ae0978
Andrea Bianchi (agap2) <scudobravo@hotmail.com>
2025-06-05 17:40:44 +0200

    Fix disclaimer VI

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TwilioController;
 use App\Models\Project;
+use App\Models\Scene;
 
 Route::get('/projects/{slug}', function ($slug) {
-    $project = Project::where('slug', $slug)
-        ->with(['initialScene' => function($query) {
-            $query->select('id', 'project_id', 'entry_message');
-        }])
-        ->firstOrFail();
+    $project = Project::where('slug', $slug)->firstOrFail();
+    
+    // Carica esplicitamente la scena iniziale
+    $initialScene = Scene::where('id', $project->initial_scene_id)->first();
+    $project->initialScene = $initialScene;
     
     return response()->json($project);
 });
