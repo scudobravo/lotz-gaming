16641b674048da5751e438302629fb8c684328ba
Andrea Bianchi (agap2) <scudobravo@hotmail.com>
2025-06-05 21:31:09 +0200

    Fix vite.config III

--- vite.config.js
+++ vite.config.js
@@ -64,6 +64,7 @@ export default defineConfig({
         chunkSizeWarningLimit: 1000,
         minify: 'esbuild',
         target: 'esnext',
+        assetsDir: '',
         emptyOutDir: true
     }
 });
