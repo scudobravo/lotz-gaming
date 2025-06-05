025438b172c6687510a12cda6100f6de465318b4
Andrea Bianchi (agap2) <scudobravo@hotmail.com>
2025-06-05 15:50:33 +0200

    Fix Twilio route api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TwilioController;
 
 Route::get('/projects/{project}', function (App\Models\Project $project) {
     return response()->json($project);
-}); 
+});
+
+// Twilio Webhook
+Route::post('/twilio/webhook', [TwilioController::class, 'handleIncomingMessage']); 
--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,9 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\SceneController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\CharacterController;
-use App\Http\Controllers\TwilioController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-// Twilio Webhook
-Route::post('/twilio/webhook', [TwilioController::class, 'handleIncomingMessage']);
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
