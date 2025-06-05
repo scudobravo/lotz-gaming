a2a740f4c64c8729595af9114140fef8873687b8
Andrea Bianchi (agap2) <scudobravo@hotmail.com>
2025-06-05 16:58:57 +0200

    Corretta importazione di glob per ESM

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-import glob from 'glob';
+import { glob } from 'glob';
 
 // Trova tutti i file .vue nella directory Pages
 const vueFiles = glob.sync('resources/js/Pages/**/*.vue');
