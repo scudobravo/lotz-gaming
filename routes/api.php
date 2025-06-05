f7dc0644c83e5b85526460cc685dae8d44d11738
Andrea Bianchi (agap2) <scudobravo@hotmail.com>
2025-06-05 15:23:58 +0200

    Route webhook

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TwilioController;
+
 Route::get('/projects/{project}', function (App\Models\Project $project) {
     return response()->json($project);
-}); 
+});
+
+// Twilio Webhook
+Route::post('/twilio/webhook', [TwilioController::class, 'handleIncomingMessage']); 
